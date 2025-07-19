71df1cd34ba78758b2b8cb9cf3a60f1440efda32
Nazmi17 <ramadaninazmi47@gmail.com>
2025-07-19 15:12:57 +0700

    Initial commit

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,10 @@
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <p>Semoga saya konsisten amin</p>
+        </div>
+    )
+}
+
+export default HelloWorld;
